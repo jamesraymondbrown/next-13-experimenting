c4c1e67e5cbb98289e5173abc3d41a144082c3f0
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-06 14:12:00 -0800

    add a Movies component

---
+++ app/Movie.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Movie({ title, id, poster_path, release_date }) {
+  const imagePath = "https://image.tmdb.org/t/p/original";
+  console.log(poster_path);
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{release_date}</h2>
+      <Link href={`/asd`}>
+        <img
+          src={imagePath + poster_path}
+          alt={`poster${id}`}
+          width="800"
+          height="800"
+        />
+      </Link>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,27 @@
-export default function Home() {
+// "use client";
+// import { useState } from "react"
+import Movie from "./Movie";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log(res);
   return (
     <main>
       <h1 className="">Hello world 🔥</h1>
+      {res.results.map((movie) => (
+        <div>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        </div>
+      ))}
     </main>
   );
 }
