1b626533e9a3836fe679eed126713bf6e3c86f7a
James Brown <114964214+jamesraymondbrown@users.noreply.github.com>
2023-03-06 13:09:09 -0800

    add tailwind

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1>Hello world 🔥</h1>
+      <h1 className="">Hello world 🔥</h1>
     </main>
   );
 }
